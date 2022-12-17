7543fa4d3ea0186f647ee5889a46f6157832614b
xxFroggyxx <xfroggyx@misiu.stargard.pl>
2022-12-17 01:24:57 +0100

    feat: add supabase

---
+++ src/lib/supabase.ts
@@ -0,0 +1,8 @@
+import { createClient } from '@supabase/supabase-js';
+
+import type { Database } from '../types/db.types';
+
+const supabaseUrl: string = process.env.NEXT_PUBLIC_SUPABASE_URL || '';
+const supabaseServerKey: string = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || '';
+
+export const supabase = createClient<Database>(supabaseUrl, supabaseServerKey);
---
+++ src/types/db.types.ts
@@ -0,0 +1,43 @@
+export type Json = string | number | boolean | null | { [key: string]: Json } | Json[];
+
+export interface Database {
+  public: {
+    Tables: {
+      'emoji-store': {
+        Row: {
+          id: number;
+          emoji: string;
+          count: number;
+        };
+        Insert: {
+          id?: number;
+          emoji: string;
+          count: number;
+        };
+        Update: {
+          id?: number;
+          emoji?: string;
+          count?: number;
+        };
+      };
+    };
+    Views: {
+      [_ in never]: never;
+    };
+    Functions: {
+      install_available_extensions_and_test: {
+        Args: Record<PropertyKey, never>;
+        Returns: boolean;
+      };
+    };
+    Enums: {
+      [_ in never]: never;
+    };
+  };
+}
+
+export interface supabaseTypes {
+  id: number;
+  emoji: string;
+  count: number;
+}
