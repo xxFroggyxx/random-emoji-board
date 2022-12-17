a8e238d7bf4262729e2fbc126428d947136ca077
xxFroggyxx <xfroggyx@misiu.stargard.pl>
2022-12-17 04:18:54 +0100

    feat: create custom hook useScoreboard v2

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,4 @@
 import Head from 'next/head';
-import { useEffect, useState } from 'react';
 
 import { Board } from '@/components/Board';
 import { Header } from '@/components/Header';
@@ -9,49 +8,15 @@ import { Scoreboard } from '@/components/Scoreboard';
 import type { supabaseTypes } from '@/types/db.types';
 import { supabase } from '@/lib/supabase';
 import { sortArray } from '@/helpers/sortArray';
+import { useScoreboard } from '@/hooks/useScoreboard';
 
 interface HomeProps {
   sortedData: supabaseTypes[];
 }
 
-export const getServerSideProps = async () => {
-  const { data: emojistore, error } = await supabase.from('emoji-store').select('*');
-
-  if (error) {
-    console.error(error);
-    throw new Error(`Problem z bazą danych`);
-  }
-
-  const sortedData = sortArray(emojistore);
-
-  return { props: { sortedData } };
-};
-
 export default function Home({ sortedData }: HomeProps) {
   const { points, setPoints, history, updateHistory } = usePointWithHistory();
-  const [scoreboard, setScoreboard] = useState<supabaseTypes[]>(sortedData);
-
-  useEffect(() => {
-    const channel = supabase
-      .channel('schema-db-changes')
-      .on('postgres_changes', { event: 'INSERT', schema: 'public' }, (payload) => {
-        const newEmoji = payload.new as supabaseTypes;
-        setScoreboard((storedEmoji) => sortArray([...storedEmoji, newEmoji]));
-      })
-      .on('postgres_changes', { event: 'UPDATE', schema: 'public' }, (payload) => {
-        setScoreboard((storedEmoji) => {
-          const temp = [...storedEmoji];
-          const findValueIndex = temp.findIndex((elm) => elm['id'] === payload.new['id']);
-          temp[findValueIndex]['count'] = payload.new['count'];
-          return sortArray(temp);
-        });
-      })
-      .subscribe();
-
-    return () => {
-      supabase.removeChannel(channel);
-    };
-  }, []);
+  const { scoreboard } = useScoreboard({ sortedData });
 
   return (
     <div>
@@ -67,3 +32,16 @@ export default function Home({ sortedData }: HomeProps) {
     </div>
   );
 }
+
+export const getServerSideProps = async () => {
+  const { data: emojistore, error } = await supabase.from('emoji-store').select('*');
+
+  if (error) {
+    console.error(error);
+    throw new Error('Cannot get data from database.');
+  }
+
+  const sortedData = sortArray(emojistore);
+
+  return { props: { sortedData } };
+};
