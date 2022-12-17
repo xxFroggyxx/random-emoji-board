b2366b4498cafe4316e16a9311be1c49a977a0a6
xxFroggyxx <xfroggyx@misiu.stargard.pl>
2022-12-17 03:25:32 +0100

    refactor: do better sorting

--- src/helpers/sortArray.ts
+++ src/helpers/sortArray.ts
@@ -1,5 +1,10 @@
 import type { supabaseTypes } from '@/types/db.types';
 
 export const sortArray = (arr: supabaseTypes[]) => {
-  return arr.sort((a, b) => b['count'] - a['count']);
+  return arr.sort((a, b) => {
+    if (a.count === b.count) {
+      return a.emoji > b.emoji ? 1 : -1;
+    }
+    return b.count - a.count;
+  });
 };
